4f79e2bb796727fa1414fc292d6735ab3d2837a7
Marco La Rosa <m@lr.id.au>
2023-05-04 14:01:21 +1000

    configure vite for ondemand import of element plus

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import commonjs from "vite-plugin-commonjs";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
 //
 // https://vitejs.dev/config/
 export default defineConfig({
     // plugins: [commonjs(), vue()],
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        // ...
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+    ],
     optimizeDeps: {
-        // entries: ["node_modules/@describo/crate-builder-component/**/*.js"],
-        // include: ["@describo/crate-builder-component"],
+        include: ["element-plus", "lodash", "@describo/crate-builder-component"],
     },
 });
