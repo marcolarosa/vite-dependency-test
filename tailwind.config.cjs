669254bfedb1a45677ae5c8935bafee94f25133a
Marco La Rosa <m@lr.id.au>
2023-05-04 14:02:51 +1000

    add tailwind config

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    future: {},
+    mode: "jit",
+    content: [
+        "./src/**/*.html",
+        "./src/**/*.{js,jsx,ts,tsx,vue}",
+        "./node_modules/@describo/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+};
